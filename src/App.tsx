fcd57fe9a05a32ec943542cf71d1085b6684d406
Garin Yaroslav <garinyaroslav13@mail.ru>
2024-01-09 07:54:03 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useAppSelector } from './redux/hooks';
 
-import { Wrapper, Sidebar } from './components';
+import { Wrapper, Sidebar, Info, Plan, AddOns, Summary, Thanking } from './components';
+
+import './App.scss';
 
 export const App: React.FC = () => {
+  const { page } = useAppSelector((state) => state.app);
+
   return (
     <Wrapper>
-      <Sidebar />
+      <>
+        <Sidebar />
+        <main className="container">
+          {page === 1 ? (
+            <Info />
+          ) : page === 2 ? (
+            <Plan />
+          ) : page === 3 ? (
+            <AddOns />
+          ) : page === 4 ? (
+            <Summary />
+          ) : page === 5 ? (
+            <Thanking />
+          ) : null}
+        </main>
+      </>
     </Wrapper>
   );
 };
